2b8a925369af2e9a033f80312dfa1dcbdd4ea5d2
Phaide <33222654+Phaide@users.noreply.github.com>
2020-05-26 12:12:24 +0200

    Add files via upload

--- Server/setup.py
+++ Server/setup.py
@@ -14,33 +14,12 @@ import subprocess
 
 from pku.config import SetupConfig
 from pku.utils import *
+from pku.core import *
 
 
 if not sys.platform.startswith("linux"):
     input("This server can only run on Linux distributions. Press enter to quit...")
     exit()
-
-
-def knockd_conf(open_sequence_file, network_interface, ssh_port):
-    current_directory = os.path.dirname(os.path.realpath(__file__))
-    return """
-[options]
-    logfile     = {current_directory}/knockd.log
-    interface   = {network_interface}
-
-[opencloseSSH]
-    one_time_sequences      = {open_sequence_file}
-    seq_timeout             = 5
-    start_command           = /sbin/iptables -I INPUT -s %IP% -p tcp --dport {ssh_port} -j ACCEPT
-    tcpflags                = syn
-    cmd_timeout             = 5
-    stop_command            = /sbin/iptables -D INPUT -s %IP% -p tcp --dport {ssh_port} -j ACCEPT
-""".format(
-        open_sequence_file=open_sequence_file,
-        network_interface=network_interface,
-        ssh_port=ssh_port,
-        current_directory=current_directory,
-    )
 
 
 def install_package(packet_manager, service: str):
@@ -51,23 +30,12 @@ def install_package(packet_manager, service: str):
         return False
 
 
-def configure_knockd(conf: str) -> None:
-    with open(SetupConfig.knockd_config_file, "w") as conf_file:
-        conf_file.write(conf)
-
-
 def main():
     install_package(
         SetupConfig.packet_manager,
         "knockd"
     )
-    configure_knockd(
-        knockd_conf(
-            SetupConfig.open_sequence_file,
-            SetupConfig.network_interface,
-            SetupConfig.ssh_port
-        )
-    )
+    configure_knockd(knockd_conf([100, 200, 300]))
     start_service("knockd")
 
 
